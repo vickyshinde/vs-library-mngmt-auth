5ea91f49f0b950da4d49697fee7a2be09a2709d6
Vicky Shinde <vicky.shinde@weboniselab.com>
2024-07-10 12:41:12 +0530

    save isAuthenticated in localStorage

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,25 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+  useEffect(() => {
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    if (storedAuth) {
+      setIsAuthenticated(JSON.parse(storedAuth));
+    }
+  }, []);
+
   const signIn = () => {
     setIsAuthenticated(true);
+    localStorage.setItem('isAuthenticated', JSON.stringify(true));
   };
 
   const signOut = () => {
     setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated');
   };
 
   return (
