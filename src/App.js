b0aefe00d9d0163e2610e42601ea46a3dc0c0429
Vicky Shinde <vicky.shinde@weboniselab.com>
2024-07-10 13:49:30 +0530

    change the flow home -> sign-in -> auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
-import SignIn from './components/SignIn';
-import BookList from './components/BookList';
-import BookDetail from './components/BookDetail';
-import { useAuth } from './context/AuthContext';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
+import SignIn from "./components/SignIn";
+import BookList from "./components/BookList";
+import BookDetail from "./components/BookDetail";
+import { useAuth } from "./context/AuthContext";
+import Home from "./components/Home";
 
 const RequireAuth = ({ children }) => {
   const auth = useAuth();
@@ -12,49 +19,58 @@ const RequireAuth = ({ children }) => {
 
 const App = () => {
   const auth = useAuth();
+  console.log("🚀 ~ RequireAuth ~ auth.isAuthenticated:", auth.isAuthenticated)
+  // console.log("🚀 ~ App ~ auth:", auth)
   return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {auth.isAuthenticated ? (
               <li>
-                <Link to="/">Sign In</Link>
+                <Link to="/books">Book List</Link>
               </li>
-              {auth.isAuthenticated && (
-                <li>
-                  <Link to="/books">Book List</Link>
-                </li>
-              )}
-            </ul>
-          </nav>
-          {auth.isAuthenticated && (
-            <div>
-              <p>Welcome, {auth.user.username}</p>
-              <button onClick={auth.signOut}>Sign Out</button>
-            </div>
-          )}
+            ) : (
+              <li>
+                <Link to="/sign-up">Sign In</Link>
+              </li>
+            )}
+          </ul>
+        </nav>
+        {auth.isAuthenticated && (
+          <div>
+            <p>Welcome, {auth.user.username}</p>
+            <button onClick={auth.signOut}>Sign Out</button>
+          </div>
+        )}
 
-          <Routes>
-            <Route path="/" element={<SignIn />} />
-            <Route
-              path="/books"
-              element={
-                <RequireAuth>
-                  <BookList />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/books/:id"
-              element={
-                <RequireAuth>
-                  <BookDetail />
-                </RequireAuth>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {!auth.isAuthenticated && (
+            <Route path="/sign-up" element={<SignIn />} />
+          )}
+          <Route
+            path="/books"
+            element={
+              <RequireAuth>
+                <BookList />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/books/:id"
+            element={
+              <RequireAuth>
+                <BookDetail />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
